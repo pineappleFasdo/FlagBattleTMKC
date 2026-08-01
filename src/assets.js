d9f9cc8e6897f96d8dfedd28dbdac08a3753b4ae
pineappleFasdo <77715312+pineappleFasdo@users.noreply.github.com>
2026-08-01 23:18:42 +0530

    Update assets.js

--- src/assets.js
+++ src/assets.js
@@ -1,37 +1,51 @@
-export const FLAG_LIST = [
-  "IN",
-  "US",
-  "JP",
-  "CN",
-  "GB",
-  "FR",
-  "DE",
-  "BR",
-  "CA",
-  "AU",
-  "IT",
-  "ES",
-  "RU",
-  "KR",
-  "MX",
-  "ZA",
-  "AR",
-  "TR",
-  "ID",
-  "SA"
+export const COLORS = [
+
+    0xff5252,
+    0xff9800,
+    0xffeb3b,
+    0x8bc34a,
+    0x4caf50,
+    0x00bcd4,
+    0x2196f3,
+    0x3f51b5,
+    0x673ab7,
+    0x9c27b0,
+    0xe91e63,
+    0x795548,
+    0x607d8b,
+    0xffffff,
+    0xffc107,
+    0x26c6da,
+    0xab47bc,
+    0xef5350,
+    0x66bb6a,
+    0x42a5f5
+
 ];
 
-export const COLORS = [
-    0xff5252,
-    0x42a5f5,
-    0x66bb6a,
-    0xffca28,
-    0xab47bc,
-    0xff7043,
-    0x26c6da,
-    0xec407a,
-    0x8d6e63,
-    0x7e57c2,
-    0x26a69a,
-    0x5c6bc0
+export const TOKEN_RADIUS = 20;
+
+export const TOKEN_DIAMETER = TOKEN_RADIUS * 2;
+
+export const ARENA_GAP_DEGREES = 50;
+
+export const WALL_SEGMENTS = 72;
+
+export const INITIAL_FLAGS = 50;
+
+export const COUNTDOWN = 60;
+
+export const FLAG_LIST = [
+
+    "IN",
+    "US",
+    "JP",
+    "CN",
+    "GB",
+    "FR",
+    "DE",
+    "BR",
+    "CA",
+    "AU"
+
 ];
