2bea5bca83c5f9bb044a59f9b13428670c74adf9
pineappleFasdo <77715312+pineappleFasdo@users.noreply.github.com>
2026-08-01 22:39:15 +0530

    Update countries.js

--- src/countries.js
+++ src/countries.js
@@ -1,3 +1,14 @@
-import { FLAG_LIST } from "./assets.js";
+const countries = [
+  { code: "IN", name: "India" },
+  { code: "US", name: "United States" },
+  { code: "JP", name: "Japan" },
+  { code: "CN", name: "China" },
+  { code: "GB", name: "United Kingdom" },
+  { code: "FR", name: "France" },
+  { code: "DE", name: "Germany" },
+  { code: "BR", name: "Brazil" },
+  { code: "CA", name: "Canada" },
+  { code: "AU", name: "Australia" }
+];
 
-export default FLAG_LIST;
+export default countries;
