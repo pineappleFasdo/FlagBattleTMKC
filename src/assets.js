68fcbd71a6c107d2365c0c0155c9cc171cdda731
pineappleFasdo <77715312+pineappleFasdo@users.noreply.github.com>
2026-08-01 22:31:37 +0530

    Create assets.js

---
+++ src/assets.js
@@ -0,0 +1,37 @@
+export const FLAG_LIST = [
+  "IN",
+  "US",
+  "JP",
+  "CN",
+  "GB",
+  "FR",
+  "DE",
+  "BR",
+  "CA",
+  "AU",
+  "IT",
+  "ES",
+  "RU",
+  "KR",
+  "MX",
+  "ZA",
+  "AR",
+  "TR",
+  "ID",
+  "SA"
+];
+
+export const COLORS = [
+    0xff5252,
+    0x42a5f5,
+    0x66bb6a,
+    0xffca28,
+    0xab47bc,
+    0xff7043,
+    0x26c6da,
+    0xec407a,
+    0x8d6e63,
+    0x7e57c2,
+    0x26a69a,
+    0x5c6bc0
+];
